7ebcf4c6d4b4bf5a41da374e55f8de5d05002320
prodsaru <junran.wang393@gmail.com>
2025-12-06 16:25:15 +0800

    Update encrypt_decrypt.py

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -4,31 +4,27 @@ from Crypto.Random import get_random_bytes
 
 class encrypt_decrypt:
 
-    def __init__(self, file):
-        self.file = file
-        self.ciphertext = ""
-        self.decrypted_data = ""
-
-    def en(self):
+    def en(file):
         key = get_random_bytes(16)  
         iv = get_random_bytes(16)   
 
         cipher = AES.new(key, AES.MODE_CBC, iv)
 
-        data = self.file.encode('utf-8')
+        data = file.encode('utf-8')
 
         padded_data = pad(data, AES.block_size)
 
-        self.ciphertext = cipher.encrypt(padded_data)
+        ciphertext = cipher.encrypt(padded_data)
 
-        cipher_dec = AES.new(key, AES.MODE_CBC, iv)
-        decrypted_padded_data = cipher_dec.decrypt(self.ciphertext)
-
-        self.decrypted_data = unpad(decrypted_padded_data, AES.block_size)
-
-    def de(self):
-        return self.decrypted_data.decode('utf-8')
+        return ciphertext
     
-    def output_encrypt(self):
-        return self.ciphertext
+    def de(ciphertext, key, iv):
+        iv = get_random_bytes(16)
+        cipher_dec = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_padded_data = cipher_dec.decrypt(ciphertext)
 
+        decrypted_data = unpad(decrypted_padded_data, AES.block_size)
+
+
+        return decrypted_data.decode('utf-8')
+    
